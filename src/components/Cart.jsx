4eeb7ea8f485ccb752eb431ebe47eed5034351f2
yunushangoksu <yunushangoksu@gmail.com>
2023-08-17 06:21:14 +0300

    local storage

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,15 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-function Cart() {
+function Cart(data) {
   const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    localStorage.setItem("cartItems", JSON.stringify(cart));
+  }, [cart]);
 
   return (
     <div className="cartWrapper">
       <div className="cartTopRow">
-        <div>3 Games</div>
+        <div>{data.length} Games</div>
         <div>Clear</div>
       </div>
-      
+
       <div className="cartItemWrapper">
         <div className="cardItemLeft">
           <img src="src/assets/borderlands3.jpg" alt="foto" />
@@ -17,11 +21,12 @@ function Cart() {
         <div className="cartItemRight">
           <div className="cartItemDelete">X</div>
           <div className="cartItemRight2">
-            <div className="cartItemName">Remnant 2</div>
+            <div className="cartItemName">{data.name}</div>
             <div>19.98</div>
           </div>
         </div>
       </div>
+      <button onClick={() => setCart([...cart, "Deneme"])}>Hebe</button>
     </div>
   );
 }
