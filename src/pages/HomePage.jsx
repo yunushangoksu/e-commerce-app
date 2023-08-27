d4980f923c8ab0df8a94d3d5abd48f356d1374d6
yunushan <yunushangoksu@gmail.com>
2023-08-27 21:33:46 +0300

    added footer

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,9 +3,25 @@ import { useState, useEffect } from "react";
 
 function HomePage() {
   return (
-    <div className="homeWrapper">
-      <HomeCategoryNavbar />
-    </div>
+    <>
+      <div className="homeWrapper">
+        <HomeCategoryNavbar />
+      </div>
+      <div className="footer">
+        <div className="footerText">
+          Made by
+          <a className="yunushan" href="https://github.com/yunushangoksu">
+            Yunushan Göksu
+          </a>
+        </div>
+        <div className="footerText">
+          Powered by
+          <a className="rawg" href="https://rawg.io/">
+            RAWG
+          </a>
+        </div>
+      </div>
+    </>
   );
 }
 
